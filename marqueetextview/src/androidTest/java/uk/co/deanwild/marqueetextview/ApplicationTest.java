15e4e27a0b2e6874000f9c350ceed8486d52c3c6
Dean Wild <dean.wild@wizix.com>
2016-03-15 11:39:20 +0000

    fix package name

--- marqueetextview/src/androidTest/java/uk/co/deanwild/marqueetextview/ApplicationTest.java
+++ marqueetextview/src/androidTest/java/uk/co/deanwild/marqueetextview/ApplicationTest.java
@@ -1,4 +1,4 @@
-package uk.co.deanwild.marqueetextviewsample;
+package uk.co.deanwild.marqueetextview;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- marqueetextview/src/main/java/uk/co/deanwild/marqueetextview/MarqueeTextView.java
+++ marqueetextview/src/main/java/uk/co/deanwild/marqueetextview/MarqueeTextView.java
@@ -1,4 +1,4 @@
-package uk.co.deanwild.marqueetextviewsample;
+package uk.co.deanwild.marqueetextview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
